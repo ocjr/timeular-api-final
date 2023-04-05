39e6fe73ebb7a31bec1097d8537649c7e1793cc9
Joe O'Connell <joconnell@frontlineed.com>
2023-04-05 09:25:32 -0700

    Updated install_requires in setup.py

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='joesTime',
-    version='0.7',
+    version='0.8',
     packages=find_packages('src'),
     package_dir={'': 'src'},
     install_requires=[
@@ -11,7 +11,8 @@ setup(
         'typing',
         'datetime',
         'ipython',
-        'black'
+        'black',
+        'pytest'
     ],
     tests_require=[
         'unittest',
