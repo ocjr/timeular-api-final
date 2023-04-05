204091d157a373897f7328db98c8aa299307f69e
Joe O'Connell <joconnell@frontlineed.com>
2023-04-05 10:17:39 -0700

    Updated Actions and Tests

--- tests/test_api.py
+++ tests/test_api.py
@@ -1,3 +1,4 @@
+import os
 import unittest
 import pytest
 from unittest.mock import patch, Mock
@@ -6,22 +7,32 @@ from src.joesTime.utils.entry import get_entry_by_id, get_entry_by_timestamp
 from src.joesTime.api import login, logout
 
 
+
+def test_logout(token):
+    # Test that logout function returns 'success'
+    assert logout(token) == "success"
+
+
 class TestAPIFunctions(unittest.TestCase):
     @patch("requests.request")
-    def test_get_entry_by_id(self, mock_request):
+    def test_get_entry_by_id(self, mock_request,mock_getpass):
         mock_response = Mock()
         mock_response.status_code = 200
         mock_response.text = '{"id": "12345", "description": "test entry"}'
         mock_request.return_value = mock_response
 
+        mock_getpass.side_effect = [os.environ['MY_APIKEY'], os.environ['MY_APISECRET']]
+        token = login()
+        assert login()["token"] is not None
+
         # Call the function with mock data
-        data = get_entry_by_id("12345", "dummy_token")
+        data = get_entry_by_id("12345", token)
 
         # Check that the mock request was called with the correct arguments
         mock_request.assert_called_with(
             "GET",
             "https://api.timeular.com/api/v3/time-entries/12345",
-            headers={"Authorization": "Bearer dummy_token"},
+            headers={"Authorization": token},
             data={},
         )
 
@@ -29,18 +40,25 @@ class TestAPIFunctions(unittest.TestCase):
         expected_data = {"id": "12345", "description": "test entry"}
         self.assertEqual(data, expected_data)
 
+        assert logout(token) == "success"
+
+
     @patch("requests.request")
-    def test_get_entry_by_timestamp(self, mock_request):
+    def test_get_entry_by_timestamp(self, mock_request, mock_getpass):
         mock_response = Mock()
         mock_response.status_code = 200
         mock_response.text = '[{"id": "12345", "description": "test entry 1"}, {"id": "67890", "description": "test entry 2"}]'
         mock_request.return_value = mock_response
 
+        mock_getpass.side_effect = [os.environ['MY_APIKEY'], os.environ['MY_APISECRET']]
+        token = login()
+        assert login()["token"] is not None
+
         # Call the function with mock data
         start_time = dt.datetime(2023, 3, 30, 0, 0, 0)
         end_time = dt.datetime(2023, 3, 31, 0, 0, 0)
         data = get_entry_by_timestamp(
-            start_time, end_time, "dummy_token", "America/New_York"
+            start_time, end_time, token, "America/New_York"
         )
 
         # Check that the mock request was called with the correct arguments
@@ -48,7 +66,7 @@ class TestAPIFunctions(unittest.TestCase):
         mock_request.assert_called_with(
             "GET",
             expected_url,
-            headers={"Authorization": "Bearer dummy_token"},
+            headers={"Authorization": token},
             data={},
         )
 
@@ -59,20 +77,4 @@ class TestAPIFunctions(unittest.TestCase):
         ]
         self.assertEqual(data, expected_data)
 
-
-@pytest.fixture
-def token():
-    token_dict = login()
-    if "token" not in token_dict:
-        raise ValueError("token not found in login response")
-    return token_dict["token"]
-
-
-def test_login():
-    # Test that login function returns a token
-    assert login()["token"] is not None
-
-
-def test_logout(token):
-    # Test that logout function returns 'success'
-    assert logout(token) == "success"
+        assert logout(token) == "success"
