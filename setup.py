d9a4e6741c41cc24b9f394509af2555b8aeae6c3
Joe O'Connell <joconnell@frontlineed.com>
2023-04-05 10:24:51 -0700

    Updated Actions Workflow

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='joesTime',
-    version='0.9',
+    version='0.10',
     packages=find_packages('src'),
     package_dir={'': 'src'},
     install_requires=[
